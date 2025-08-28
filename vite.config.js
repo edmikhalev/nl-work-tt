f543701db9047b25229cefcbf8133561bfe62f95
edmihaleff <ed.mihaleff@ya.ru>
2025-08-28 08:15:53 +0300

    feat: gh-conf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +10,12 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	build: {
+		outDir: 'dist', // Output directory
+		rollupOptions: {
+			input: resolve(__dirname, 'index.html'), // Entry point for the build
 		},
 	},
 	server: {
